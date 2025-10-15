c1c2a96eb03488cfddf88f8a3efdeef918ab5fd0
230676 <230676@si.douane.gov.dz>
2025-10-15 07:50:51 +0100

    insert varchar only 2,445 rows affected in 21 ms

--- psql/2_insert_varchars.sql
+++ psql/2_insert_varchars.sql
@@ -1,3 +1,4 @@
+SET lc_time = 'fr_FR.UTF-8';
 
 INSERT INTO rend25.rendement (
     id, mois, annee, dr, idd, brigade, wilaya, collab,
@@ -18,8 +19,14 @@ SELECT
     NULLIF(loc_e, '0')::numeric(10,6),
     NULLIF(loc_n, '0')::numeric(10,6),
     topo,
-    NULLIF("date", '')::date,
-    NULLIF(heure, '')::time,
+    CASE
+        WHEN trim("date") = '' OR "date" IS NULL THEN NULL
+        ELSE to_date("date", 'DD TMMonth YYYY')
+        END AS date,
+    CASE
+        WHEN trim(heure) = '' OR heure IS NULL THEN NULL
+        ELSE heure::time
+        END AS heure,
     NULLIF(armes_u, '0')::int,
     type_armes,
     NULLIF(mun_u, '0')::int,
@@ -36,3 +43,32 @@ SELECT
     NULLIF(alim_kg, '0')::numeric(12,2),
     NULLIF(alim_l, '0')::numeric(12,2)
 FROM rend25.rendement_;
+
+
+INSERT INTO rend25.rendement (
+    id,
+    mois,
+    annee,
+    dr,
+    idd,
+    brigade,
+    wilaya,
+    collab,
+    topo,
+    type_armes,
+    desc_prod
+)
+SELECT
+    id::int,
+    mois,
+    NULLIF(annee, '0')::int,
+    dr,
+    idd,
+    brigade,
+    wilaya,
+    collab,
+    topo,
+    type_armes,
+    desc_prod
+FROM rend25.rendement_;--[2025-10-15 07:50:09] 2,445 rows affected in 21 ms
+
